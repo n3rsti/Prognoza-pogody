f0e2b2d0d3164dee4d38966c351111822d6714eb
Krzysztof <krzysiek.witucki55@gmail.com>
2018-12-28 14:28:39 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+import time
+
+localtime = time.localtime(time.time())
+city_input = input("Podaj nazwę miasta: ")
+adress = "http://api.openweathermap.org/data/2.5/weather?appid=8c1478e1474662cfbc44dd0c4a281a58&q=" + city_input
+req = requests.get(adress).json()
+weather = req["weather"][0]["main"]
+temp = req["main"]["temp"]
+wind = req["wind"]["speed"]
+city = req["name"]
+
+plik = open("Pogoda.txt", "w")
+plik.write("Dnia " + time.strftime("%Y-%m-%d", time.localtime()) + " w mieście " + city + " temperatura wynosi " + str(temp) + ", a wiatr wieje z prędkością " + str(wind) + ".")
+plik.close()
+
+print("Wykonano")
