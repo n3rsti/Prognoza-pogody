0ff8226202ec4a111de3c153648d927cdfa614aa
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2019-06-11 11:25:53 +0200

    formatted API request from weather forecast

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import time
 
 localtime = time.localtime(time.time())
 city_input = input("Podaj nazwę miasta: ")
-adress = "http://api.openweathermap.org/data/2.5/weather?appid=8c1478e1474662cfbc44dd0c4a281a58&q=" + city_input
+adress = 'http://api.openweathermap.org/data/2.5/weather?appid=8c1478e1474662cfbc44dd0c4a281a58&q={}'.format(city_input)
 req = requests.get(adress).json()
+
 weather = req["weather"][0]["main"]
 temp = req["main"]["temp"]
 wind = req["wind"]["speed"]
 city = req["name"]
+formatted_temp = int(temp - 273)
+time = time.strftime("%Y-%m-%d", time.localtime())
 
-plik = open("Pogoda.txt", "w")
-plik.write("Dnia " + time.strftime("%Y-%m-%d", time.localtime()) + " w mieście " + city + " temperatura wynosi " + str(int(temp - 273.15)) + " stopni Celsjusza, a wiatr wieje z prędkością " + str(wind) + " m/s .")
-plik.close()
 
-print("Wykonano")
+print('{} in {} it is {} , the temperature is {}℃ and wind speed is {} m/s .'.format(time, city, weather, formatted_temp, wind))
+
+
+
